f6376fdaec296c7c96f4b5842b8ba47e99f57d35
Julio Garcia <garguidevsw@gmail.com>
2024-01-08 21:51:26 -0600

    Firebase Cloud Messaging with Flutter + BLoC

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pushapp/config/router/app_router.dart';
+import 'package:pushapp/config/theme/app_theme.dart';
+import 'package:pushapp/presentation/blocs/notifications/notifications_bloc.dart';
+
+void main() async {
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await NotificationsBloc.initializeFCM();
+
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (_) => NotificationsBloc(),
+      ),
+    ],
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Push Notification',
+      theme: AppTheme().getTheme(),
+      routerConfig: appRouter,
+      builder: (context, child) => HandleNotificationInteraction(child: child!),
+    );
+  }
+}
+
+class HandleNotificationInteraction extends StatefulWidget {
+  final Widget child;
+  const HandleNotificationInteraction({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  State<HandleNotificationInteraction> createState() =>
+      _HandleNotificationInteractionState();
+}
+
+class _HandleNotificationInteractionState
+    extends State<HandleNotificationInteraction> {
+  Future<void> setupInteractedMessage() async {
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    if (initialMessage != null) {
+      _handleMessage(initialMessage);
+    }
+
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  }
+
+  void _handleMessage(RemoteMessage message) {
+    context.read<NotificationsBloc>().handleRemoteMessage(message);
+
+    final messageId =
+        message.messageId?.replaceAll(':', '').replaceAll('%', '');
+    appRouter.push('/push-details/$messageId');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupInteractedMessage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
---
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -0,0 +1,111 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:pushapp/domain/entities/push_message.dart';
+import 'package:pushapp/firebase_options.dart';
+
+part 'notifications_event.dart';
+part 'notifications_state.dart';
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationsBloc() : super(const NotificationsState()) {
+    on<NotificationStatusChanged>(_notificationStatusChanged);
+
+    on<NotificationReceived>(_notificationReceived);
+
+    // Verificar status de las notificaciones
+    _initialStatusCheck();
+
+    // Listener para notificaciones en foreground
+    _onForegroundMessage();
+  }
+
+  static Future<void> initializeFCM() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  void _notificationStatusChanged(
+      NotificationStatusChanged event, Emitter<NotificationsState> emit) {
+    emit(state.copyWith(status: event.status));
+    _getFCMToken();
+  }
+
+  void _notificationReceived(
+      NotificationReceived event, Emitter<NotificationsState> emit) {
+    emit(state
+        .copyWith(notifications: [event.notification, ...state.notifications]));
+  }
+
+  void _initialStatusCheck() async {
+    final settings = await messaging.getNotificationSettings();
+
+    add(NotificationStatusChanged(settings.authorizationStatus));
+  }
+
+  void _getFCMToken() async {
+    if (state.status != AuthorizationStatus.authorized) return;
+
+    final token = await messaging.getToken();
+
+    print('FCM Token: $token');
+  }
+
+  void handleRemoteMessage(RemoteMessage message) {
+    if (message.notification == null) return;
+
+    final notification = PushMessage(
+      messageId:
+          message.messageId?.replaceAll(':', '').replaceAll('%', '') ?? '',
+      title: message.notification!.title ?? '',
+      body: message.notification!.body ?? '',
+      sentDate: message.sentTime ?? DateTime.now(),
+      data: message.data,
+      imageUrl: Platform.isAndroid
+          ? message.notification!.android?.imageUrl
+          : message.notification!.apple?.imageUrl,
+    );
+
+    add(NotificationReceived(notification));
+  }
+
+  void _onForegroundMessage() {
+    FirebaseMessaging.onMessage.listen(handleRemoteMessage);
+  }
+
+  void requestPermission() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    add(NotificationStatusChanged(settings.authorizationStatus));
+  }
+
+  PushMessage? getMessageById(String messageId) {
+    final exist =
+        state.notifications.any((element) => element.messageId == messageId);
+
+    if (!exist) return null;
+
+    return state.notifications
+        .firstWhere((element) => element.messageId == messageId);
+  }
+}
